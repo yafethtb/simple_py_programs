48f0aad3ce7e5acaddd30e4813d4667514d3a64d
Yafeth Tandi Bendon <yafethtb@gmail.com>
2021-12-06 14:14:36 +0800

    Higher or Lower game
    I made these to re-create game Higher or Lower (about the game: http://www.higherlowergame.com/) as my way to tackle the day 14 challenge of "100 days of Code" course by Angela Yu (https://www.udemy.com/course/100-days-of-code). 
    
    The data I used came from Food Nutrition Information by Adam Helsinger in Data.world (https://data.world/adamhelsinger/food-nutrition-information/)
    
    The excel spreadsheed (food_data.xlsx) was processed by me from the original file by Adam Helsinger (https://data.world/adamhelsinger/food-nutrition-information/file/NutritionalFacts_Fruit_Vegetables_Seafood.csv)
    
    Feel free to use these files as you like.

---
+++ HIGHER_OR_LOWER/data_dict.py
@@ -0,0 +1,190 @@
+''' Created with data provided freely by Adam Helsinger in data.world.
+https://data.world/adamhelsinger/food-nutrition-information/'''
+
+food_list = [{
+    'Name': 'Apple, 1 large (242 g/8 oz)',
+    'Calories': 130
+}, {
+    'Name': 'Asparagus, 5 spears (93 g/3.3 oz)',
+    'Calories': 20
+}, {
+    'Name': 'Avocado, California,1/5 medium (30 g/1.1 oz)',
+    'Calories': 50
+}, {
+    'Name': 'Banana, 1 medium (126 g/4.5 oz)',
+    'Calories': 110
+}, {
+    'Name': 'Bell Pepper, 1 medium (148 g/5.3 oz)',
+    'Calories': 25
+}, {
+    'Name': 'Blue Crab',
+    'Calories': 100
+}, {
+    'Name': 'Broccoli, 1 medium stalk (148 g/5.3 oz)',
+    'Calories': 45
+}, {
+    'Name': 'Cantaloupe, 1/4 medium (134 g/4.8 oz)',
+    'Calories': 50
+}, {
+    'Name':
+    'Carrot, 1 carrot, 7" long,ﾊ1 1/4" diameter (78 g/2.8 oz)',
+    'Calories': 30
+}, {
+    'Name': 'Catfish',
+    'Calories': 130
+}, {
+    'Name': 'Cauliflower, 1/6 medium head (99 g/3.5 oz)',
+    'Calories': 25
+}, {
+    'Name': 'Celery, 2 medium stalks (110 g/3.9 oz)',
+    'Calories': 15
+}, {
+    'Name': 'Clams, about 12 small',
+    'Calories': 110
+}, {
+    'Name': 'Cod',
+    'Calories': 90
+}, {
+    'Name': 'Cucumber, 1/3 medium (99 g/3.5 oz)',
+    'Calories': 10
+}, {
+    'Name': 'Flounder/Sole',
+    'Calories': 100
+}, {
+    'Name': 'Grapefruit, 1/2 medium, (154 g/5.5 oz)',
+    'Calories': 60
+}, {
+    'Name': 'Grapes, 3/4 cup (126 g/4.5 oz)',
+    'Calories': 90
+}, {
+    'Name': 'Green (Snap) Beans, 3/4 cup cut (83 g/3.0 oz)',
+    'Calories': 20
+}, {
+    'Name': 'Green Onion, 1/4 cup chopped (25 g/0.9 oz)',
+    'Calories': 10
+}, {
+    'Name': 'GreenﾊCabbage, 1/12 medium head (84 g/3.0 oz)',
+    'Calories': 25
+}, {
+    'Name': 'Haddock',
+    'Calories': 100
+}, {
+    'Name': 'Halibut',
+    'Calories': 120
+}, {
+    'Name': 'HoneydewﾊMelon 1/10 mediumﾊmelon  (134 g/4.8 oz)',
+    'Calories': 50
+}, {
+    'Name': 'Iceberg Lettuce, 1/6 medium head (89 g/3.2 oz)',
+    'Calories': 10
+}, {
+    'Name': 'Kiwifruit, 2 medium (148 g/5.3 oz)',
+    'Calories': 90
+}, {
+    'Name': 'Leaf Lettuce, 1 1/2 cups shredded (85 g/3.0 oz)',
+    'Calories': 15
+}, {
+    'Name': 'Lemon, 1 medium (58 g/2.1 oz)',
+    'Calories': 15
+}, {
+    'Name': 'Lime, 1 medium (67 g/2.4 oz)',
+    'Calories': 20
+}, {
+    'Name': 'Lobster',
+    'Calories': 80
+}, {
+    'Name': 'Mushrooms, 5 medium (84 g/3.0 oz)',
+    'Calories': 20
+}, {
+    'Name': 'Nectarine, 1 medium (140 g/5.0 oz)',
+    'Calories': 60
+}, {
+    'Name': 'Ocean Perch',
+    'Calories': 110
+}, {
+    'Name': 'Onion, 1 medium (148 g/5.3 oz)',
+    'Calories': 45
+}, {
+    'Name': 'Orange Roughy',
+    'Calories': 80
+}, {
+    'Name': 'Orange, 1 medium (154 g/5.5 oz)',
+    'Calories': 80
+}, {
+    'Name': 'Oysters, about 12 medium',
+    'Calories': 100
+}, {
+    'Name': 'Peach, 1 medium (147 g/5.3 oz)',
+    'Calories': 60
+}, {
+    'Name': 'Pear, 1 medium (166 g/5.9 oz)',
+    'Calories': 100
+}, {
+    'Name': 'Pineapple, 2 slices, 3" diameter,ﾊ3/4" thick',
+    'Calories': 50
+}, {
+    'Name': 'Plums, 2 medium (151 g/5.4 oz)',
+    'Calories': 70
+}, {
+    'Name': 'Pollock',
+    'Calories': 90
+}, {
+    'Name': 'Potato, 1 medium (148 g/5.3 oz)',
+    'Calories': 110
+}, {
+    'Name': 'Radishes, 7 radishes (85 g/3.0 oz)',
+    'Calories': 10
+}, {
+    'Name': 'Rainbow Trout',
+    'Calories': 140
+}, {
+    'Name': 'Rockfish',
+    'Calories': 110
+}, {
+    'Name': 'Salmon, Atlantic/Coho/Sockeye /Chinook',
+    'Calories': 200
+}, {
+    'Name': 'Salmon,ﾊChum/Pink',
+    'Calories': 130
+}, {
+    'Name': 'Scallops, about 6 large or 14 small',
+    'Calories': 140
+}, {
+    'Name': 'Shrimp',
+    'Calories': 100
+}, {
+    'Name': 'Strawberries, 8 medium (147 g/5.3 oz)',
+    'Calories': 50
+}, {
+    'Name': 'SummerﾊSquash, 1/2 medium (98 g/3.5 oz)',
+    'Calories': 20
+}, {
+    'Name': 'Sweet Corn, kernels from 1ﾊmedium ear (90 g/3.2 oz)',
+    'Calories': 90
+}, {
+    'Name':
+    'Sweet Potato, 1 medium, 5" long,ﾊ2" diameter (130 g/4.6 oz)',
+    'Calories': 100
+}, {
+    'Name': 'SweetﾊCherries 21 cherries;ﾊ1 cup',
+    'Calories': 100
+}, {
+    'Name': 'Swordfish',
+    'Calories': 120
+}, {
+    'Name': 'Tangerine, 1 medium (109 g/3.9 oz)',
+    'Calories': 50
+}, {
+    'Name': 'Tilapia',
+    'Calories': 110
+}, {
+    'Name': 'Tomato, 1 medium (148 g/5.3 oz)',
+    'Calories': 25
+}, {
+    'Name': 'Tuna',
+    'Calories': 130
+}, {
+    'Name':
+    'Watermelon, 1/18 medium melon; 2 cups diced pieces(280 g/',
+    'Calories': 80
+}]
