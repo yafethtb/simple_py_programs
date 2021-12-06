985335f5c5f31cb062c62cd2ed09b8dcf53ba121
Yafeth Tandi Bendon <yafethtb@gmail.com>
2021-12-06 16:11:01 +0800

    Grouping decision part into a function
    I'm using Angela Yu's approach here. So rather than create long print statements for each conditions, I put them together into a function.
    It makes the decision part is easier to understand.
    
    And also rather than put the user input checker together with decision part, I create a block separated from the decision part. I call it "Guessing part"

--- HIGHER_OR_LOWER/hi_lo.py
+++ HIGHER_OR_LOWER/hi_lo.py
@@ -5,20 +5,41 @@ def game_over(right):
     ''' Game over. Return message how much player guess right. '''
     print(f"Game is over. Your points: {right}.")
 
+def result(name_a, name_b, cal_a, cal_b, guess):
+    ''' Checking user guess. Return only True or False '''
+    print('-' * 20)
+    print(f"{name_a} have {cal_a} Kcal.")
+    print(f"{name_b} have {cal_b} Kcal.")
+    if (guess == 'a' and cal_a > cal_b) or (guess == 'b' and cal_a < cal_b):
+        return True
+    else:
+        return False
+
 score = 0
 
+# Game part
 while True:
-    print('-' * 20)
-    fact_a, fact_b = random.sample(food_list, k=2) 
+    print('=' * 20)
+    fact_a, fact_b = random.sample(food_list, k=2)
+    fact_a_name, fact_b_name, fact_a_cal, fact_b_cal =  fact_a['Name'], fact_b['Name'], fact_a['Calories'], fact_b['Calories']
+    
+    # Guessing part
+    while True:
+        hi_lo = input(
+            f"Between {fact_a_name} and {fact_b_name}\nWhich one have more calories? Type 'a' if {fact_a_name} is higher, or 'b' if {fact_b_name} is higher:  >  "
+        ).lower()
+        if hi_lo == 'a' or hi_lo == 'b':
+            break
+        else:
+            print("Please choose only 'a' or 'b'.\n")
+            continue
+    
+    answer = result(fact_a_name, fact_b_name, fact_a_cal, fact_b_cal, hi_lo)
 
-    hi_lo = input(
-        f"Between {fact_a['Name']} and {fact_b['Name']}\nWhich one have more calories. Type 'a' if {fact_a['Name']} is higher, or 'b' if {fact_b['Name']} is higher:  >  "
-    ).lower()
-
-    if (hi_lo == 'a' and fact_a['Calories'] > fact_b['Calories']) or (hi_lo == 'b' and fact_a['Calories'] < fact_b['Calories']):
-        print(f"{fact_a['Name']} have {fact_a['Calories']} kcal")
-        print(f"{fact_b['Name']} have {fact_b['Calories']} kcal")
+    # Decision part
+    if answer is True:
         print("You are right.")
+        print('-' * 20)
         score += 1
 
         go_on = input("Continue? Press 'y' to continue. Press any other keys to stop. :  > ").lower()
@@ -26,15 +47,12 @@ while True:
             continue
         else:
             game_over(score)
-            print('-' * 20)
+            print('=' * 20)
             break
-        
-    elif (hi_lo == 'a' and fact_a['Calories'] < fact_b['Calories']) or (hi_lo == 'b' and fact_a['Calories'] > fact_b['Calories']):
-        print(f"{fact_a['Name']} have {fact_a['Calories']} kcal.")
-        print(f"{fact_b['Name']} have {fact_b['Calories']} kcal.")
-        print("You are wrong.")
-        game_over(score)
-        print('-' * 20)
-        break
+            
     else:
-        print("Please choose only 'a' or 'b'.")
+        print("\nYou are wrong.\n")
+        game_over(score)
+        print('=' * 20)
+        break          
+        
